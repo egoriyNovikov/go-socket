dab276f05f75e9165fe08d3f7871aa4efe689d3c
Egor Novikov <egoriyNovikov@gmail.com>
2026-01-19 12:25:14 +0300

    main.go code

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,10 +1,21 @@
 package main
 
 import (
-	"github.com/go-socket/internal/config"
+	"log"
+	"net/http"
+
+	http_handler "github.com/go-socket/internal/http"
+	"github.com/go-socket/internal/ws"
 )
 
 func main() {
-	config := config.LoadConfig()
+	hub := ws.NewHub()
+	go hub.Run()
+
+	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
+		http_handler.ServerWS(w, r)
+	})
+	log.Println("Server started at :8080")
+	http.ListenAndServe(":8080", nil)
 
 }
